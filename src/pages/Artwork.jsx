df4dc681facf2d21c4e44355ddd160e6d2a292a9
jayzumen <reinhardtjanniklas96@gmail.com>
2022-12-02 16:03:35 +0100

    fixed style for artwork

--- src/pages/Artwork.jsx
+++ src/pages/Artwork.jsx
@@ -25,7 +25,7 @@ function Artwork() {
     <div>
       {artwork ? (
         <div className="p-10 mx-auto xl:max-w-[1400px] xl:flex xl:justify-start xl:p-0">
-          <div className="mx-auto w-fit h-fit">
+          <div className="w-fit h-fit text-center">
             {/* Image Title */}
             <p className="text-center font-bold text-2xl py-4">
               {artwork.title}
